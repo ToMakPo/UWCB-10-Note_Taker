5ccb7624cf1e37c96b481e8fcdf4e802da14c1bf
Makai Post <postmakai@gmail.com>
2020-12-15 21:36:20 -0800

    added structure to server

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express')
+const path = require('path')
+const database = require('./lib/database')
 
-const app = express()
 const PORT = process.env.PORT || 3000
 
+const app = express()
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// app.get('/', (req, res) => {
-//     res.send('Server Homes')
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(database.getAPI())
+})
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    res.json(database.getNote(id))
+})
+
+app.post('create-note', data => database.createNote(...data))
+app.post('edit-note', data => database.editNote(...data))
+app.post('delete-note', data => database.deleteNode(...data))
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT)
